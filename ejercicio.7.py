cc2ab23e33198a404284556be291c6779b1e9d52
Villca Yair <yairvillca@gmail.com>
2025-05-18 22:48:23 -0300

    Delete ejercicio.7.py

--- ejercicio.7.py
+++
@@ -1,8 +0,0 @@
-def guardar_nombre():
-    nombre = input("Ingrese su nombre: ")
-    with open("nombres.txt", 'w') as archivo:
-        archivo.write(nombre + '\n')
-    print(f"Su nombre '{nombre}' ha sido guardado en 'nombres.txt'.")
-
-# Ejemplo de como se usaria:
-guardar_nombre()
